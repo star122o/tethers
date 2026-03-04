ac23bb992ee4c5894ce2783742923417494cddfa
Stargaze <star@kitties.email>
2026-03-04 12:58:05 +0700

    Initial commit

---
+++ src/main/kotlin/dev/star122o/tethersCore/TethersCore.kt
@@ -0,0 +1,14 @@
+package dev.star122o.tethersCore
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TethersCore : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
